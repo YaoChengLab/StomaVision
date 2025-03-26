8afd06913b535c0979e05d8df05be1084880573a
Heiru Wu <heiru.wu@instill.tech>
2025-03-27 02:53:41 +0800

    chore: update script

--- train.sh
+++ train.sh
@@ -1,12 +1,15 @@
 # Finetune p5 models with multiple GPUs
 
-batch_size=8
+batch_size=4
 
 time=$(date +%F_%T)
 
-# python seg/segment/train.py \
+# sample command for distributed training with 2 gpu
+# python -m torch.distributed.run \
+#     --nproc_per_node 2 \
+#     --sync-bn \
 #     --workers 6 \
-#     --device cpu \
+#     --device 0,1 \
 #     --batch-size $batch_size \
 #     --cfg cfg/training/yolov7-seg.yaml \
 #     --data data/$data.yaml \
@@ -15,42 +18,29 @@ time=$(date +%F_%T)
 #     --name yolov7-abrc-$data \
 #     --hyp hyp/hyp.scratch.abrc.yaml
 
-# sample command for distributed training with 2 gpu
-    # -m torch.distributed.run \
-    # --nproc_per_node 2 \
-    # --sync-bn \
-# python seg/segment/train.py \
-#     --workers 8 \
-#     --device 0 \
-#     --batch-size $batch_size \
-#     --cfg cfg/training/yolov7-seg.yaml \
-#     --data data/stomavision.yaml \
-#     --img 640 \
-#     --weights 'model/yolov7-seg.pt' \
-#     --name single-label-$time \
-#     --hyp hyp/hyp.scratch.abrc.yaml \
-#     --epochs 300
 
+# train stage 1 model
+python seg/segment/train.py \
+    --workers 8 \
+    --device 0 \
+    --batch-size $batch_size \
+    --cfg cfg/training/yolov7-seg.yaml \
+    --data data/stomavision-outerline.yaml \
+    --img 640 \
+    --weights 'model/yolov7-seg.pt' \
+    --name outerline-$time \
+    --hyp hyp/hyp.scratch.abrc.yaml \
+    --epochs 100
+
+# train stage 2 model
 python seg/segment/train.py \
     --workers 8 \
     --device 1 \
     --batch-size $batch_size \
-    --cfg cfg/training/yolov7-seg-multilabel.yaml \
-    --data data/stomavision-multilabel.yaml \
+    --cfg cfg/training/yolov7-seg.yaml \
+    --data data/stomavision-cropped.yaml \
     --img 640 \
     --weights 'model/yolov7-seg.pt' \
-    --name multi-label-$time \
-    --hyp hyp/hyp.scratch.abrc.yaml \
-    --epochs 300
-
-# Finetune p5 models with single GPU
-#python seg/segment/train.py \
-#--workers 8 \
-#--device 2 \
-#--batch-size 16 \
-#--data data/stomata200-mix.yaml \
-#--img 640 \
-#--cfg cfg/training/yolov7-seg.yaml \
-#--weights 'model/yolov7-seg.pt' \
-#--name yolov7-abrc-stomata200-mix \
-#--hyp hyp/hyp.scratch.abrc.yaml
+    --name cropped-$time \
+    --hyp hyp/hyp.scratch.abrc-cropped.yaml \
+    --epochs 100
